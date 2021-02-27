de69cf8bab7aa5439f71291cd8763f35b5db53ea
luca-arweave <luca@arweave.org>
2021-02-27 16:06:43 +0100

    removed static repo clones

--- gateway/src/utility/log.utility.ts
+++
@@ -1,8 +0,0 @@
-import {createLogger, transports, format} from 'winston';
-
-export const log = createLogger({
-  level: 'info',
-  transports: new transports.Console({
-    format: format.simple(),
-  }),
-});
